4240ef78a9b16047ffe891d4995e86130cba2d6c
snowjak88 <sjnowak88@gmail.com>
2017-12-13 15:06:00 -0500

    Reconfigured :render-mq to support external ActiveMQ broker
    This will also allows other projects to connect to the
    otherwise-internal broker.

--- rays3d/render-mq/src/main/java/org/rays3d/rendermq/App.java
+++ rays3d/render-mq/src/main/java/org/rays3d/rendermq/App.java
@@ -1,11 +1,16 @@
 package org.rays3d.rendermq;
 
+import java.util.Collection;
+
 import javax.jms.ConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.ConversionService;
@@ -43,7 +48,33 @@ public class App {
 		return builder.rootUri(renderDbUrl).requestFactory(httpRequestFactory()).build();
 	}
 
-	@Bean("activemq")
+	@ConditionalOnProperty(name = { "rays3d.rendermq.use-embedded-broker" }, havingValue = "true")
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public BrokerService brokerService(
+			@Value("${rays3d.rendermq.embedded-broker-connectors}") Collection<String> connectorURLs) throws Exception {
+
+		final BrokerService broker = new BrokerService();
+		for (String connectorURL : connectorURLs)
+			broker.addConnector(connectorURL);
+
+		broker.setPersistent(false);
+
+		return broker;
+	}
+
+	@Bean
+	public ActiveMQConnectionFactory activeMQConnectionFactory(@Value("${spring.activemq.broker-url}") String brokerURL,
+			@Value("${spring.activemq.user}") String brokerUserName,
+			@Value("${spring.activemq.password}") String brokerPassword) {
+
+		final ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerURL);
+		connectionFactory.setUserName(brokerUserName);
+		connectionFactory.setPassword(brokerPassword);
+
+		return connectionFactory;
+	}
+
+	@Bean(name = "activemq", initMethod = "start", destroyMethod = "stop")
 	public ActiveMQComponent activeMqComponent(ConnectionFactory connectionFactory) {
 
 		final ActiveMQComponent activemq = new ActiveMQComponent();
